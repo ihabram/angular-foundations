f77f04b34d8bbfca0a73b3f9ca61a714b806d49d
ihabram <habramistvan@gmail.com>
2026-08-28 17:47:54 +0200

    Add site header to main app

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'bot-site-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './site-header.component.html',
   styleUrl: './site-header.component.css'
 })
