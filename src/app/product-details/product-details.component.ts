59c40ed7f93e7bd85ebedf6780cf807eb3d73e0d
ihabram <habramistvan@gmail.com>
2026-08-28 15:52:23 +0200

    Passing data to component by input

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, input} from '@angular/core';
 import {IProduct} from '../product.model';
 import {CurrencyPipe} from '@angular/common';
 
@@ -9,20 +9,8 @@ import {CurrencyPipe} from '@angular/common';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
-  product: IProduct;
+  product = input.required<IProduct>();
   availableInventory = signal(5);
-
-  constructor() {
-    this.product = {
-      id: 2,
-      description: 'A friendly robot head with two eyes and a smile -- great for domestic use.',
-      name: 'Friendly Bot',
-      imageName: `head-friendly.png`,
-      category: 'Heads',
-      price: 945.0,
-      discount: 0.2
-    }
-  }
 
   getImageUrl(product: IProduct) {
     return `/images/robot-parts/` + product.imageName;
