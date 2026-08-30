d0932c72eb4f26aefdf90c9db4ff02fd97ddc72f
ihabram <habramistvan@gmail.com>
2026-08-30 16:23:18 +0200

    Implement simple products provider method

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {IProduct} from './product.model';
+import products from "./products.json";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-
-  constructor() { }
+  getProducts(): IProduct[] {
+    return products;
+  }
 }
